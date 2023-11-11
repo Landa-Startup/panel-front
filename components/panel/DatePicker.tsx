c22317d33b2f9d4495e14d7c0b71119d9721e2a3
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-11 12:00:37 +0330

    Chore: Fix formatting in DatePicker component.

--- components/panel/DatePicker.tsx
+++ components/panel/DatePicker.tsx
@@ -1,11 +1,13 @@
-
-import {JBDateInput} from 'jb-date-input-react';
+import { JBDateInput } from 'jb-date-input-react';
 function DatePicker() {
-
-
   return (
     <div>
-      <JBDateInput label="تاریخ" format="YYYY/MM/DD" valueType="JALALI" value="1399-05-01T12:05:39.530Z"></JBDateInput>
+      <JBDateInput
+        label="تاریخ"
+        format="YYYY/MM/DD"
+        valueType="JALALI"
+        value="1399-05-01T12:05:39.530Z"
+      ></JBDateInput>
     </div>
   );
 }
