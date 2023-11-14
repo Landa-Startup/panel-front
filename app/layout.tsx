145e95b92d04cc763c05d34ca2577642913f7203
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-14 10:38:49 +0330

    Chore: Update favicon link in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
       </head>
       <body>
         {/* <Navbar>  */}
