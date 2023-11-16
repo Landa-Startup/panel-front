932a1637d86329ad67b7d060693ed6db6a3dbb65
Sajjad Momeni <sajjad5221@gmail.com>
2023-11-16 14:39:42 +0330

    chore: update middleware rules

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,42 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { protectedRoutes } from "@/router/routes"
+import { DecodedToken } from "app/types/global";
 
 export function middleware(request: NextRequest) {
-        const currentUser = request.cookies.get("currentUser")?.value;
-        const isProtectedRoute = protectedRoutes.some(r => {
-                if(typeof r === 'string') {
-                    return request.nextUrl.pathname === r 
-                } else {
-                    // regex
-                    return r.test(request.nextUrl.pathname) 
-                }
-            })
-            
-        console.log("middleware", { currentUser, isProtectedRoute });
-        if (isProtectedRoute && currentUser === undefined) {
-                console.log("middleware", "redirecting to login");
-                const redirectUrl = new URL("/auth/login", request.nextUrl.origin).toString();
-                return NextResponse.redirect(redirectUrl);
+    const currentUser: DecodedToken = JSON.parse(request.cookies.get('currentUser')?.value || '{}');
+    const isProtectedRoute = protectedRoutes.some(r => {
+        if (typeof r === 'string') {
+            return request.nextUrl.pathname === r
+        } else {
+            // regex
+            return r.test(request.nextUrl.pathname)
         }
-        return NextResponse.next();
+    })
+
+    // Rule 1
+    if (request.nextUrl.pathname === '/') {
+        if (Object.keys(currentUser).length !== 0) {
+            const redirectUrl = new URL(`/dashboard/${currentUser?.role}`, request.nextUrl.origin).toString();
+            return NextResponse.redirect(redirectUrl)
+        } else {
+            const redirectUrl = new URL("/auth/login", request.nextUrl.origin).toString()
+            return NextResponse.redirect(redirectUrl)
+        }
+    }
+    // Rule 2
+    if (request.nextUrl.pathname === '/auth/login') {
+        if (Object.keys(currentUser).length !== 0) {
+            const redirectUrl = new URL(`/dashboard/${currentUser?.role}`, request.nextUrl.origin).toString();
+            return NextResponse.redirect(redirectUrl)
+        }
+    }
+    // Rule 3
+    if (isProtectedRoute) {
+        if (Object.keys(currentUser).length == 0) {
+            const redirectUrl = new URL("/auth/login", request.nextUrl.origin).toString();
+            return NextResponse.redirect(redirectUrl)
+        }
+    }
+
+    return NextResponse.next();
 }
