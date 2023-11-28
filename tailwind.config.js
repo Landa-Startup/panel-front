a40e672a5e93cdda5eac0147c60802a313ff6f13
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-28 11:59:04 +0330

    chore: update tailwind.config.js with new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,20 @@ module.exports = {
       fontFamily: {
         gilda: ['Gilda Display', 'serif'],
         barlow: ['Barlow', 'serif'],
-        condensed: ['Condensed', 'serif'],
+        condensed: ['Condensed', 'serif']
       },
       colors: {
         primary: '#AA8453',
         whiteGold: '#F8F5F0',
         tableHeader: '#DDCEBA',
-      },
-    },    
+        panelOrange50: '#fef8ec',
+        panelOrange100: '#fceac9',
+        panelOrange200: '#f7c873',
+        panelOrange300: '#f5b654',
+        panelOrange400: '#f39c2c',
+        panelOrange500: '#ec7a14'
+      }
+    }
   },
   plugins: [require('daisyui')],
   daisyui: {
@@ -24,6 +30,6 @@ module.exports = {
     utils: true, // adds responsive and modifier utility classes
     rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
     prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-  },
+    logs: true // Shows info about daisyUI version and used config in the console when building your CSS
+  }
 };
